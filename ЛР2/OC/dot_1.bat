e944441458804d13f31f8de23e5b790bfd7389e7
Егор Кононович <100016380+TheAmadei@users.noreply.github.com>
2022-05-18 21:54:57 +0300

    Delete ЛР2 directory

--- ЛР2/OC/dot_1.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF 
-cls 
-echo list of hidden files in a folder %1 in the file %2
-dir %1 /ah /b > D:\OC\%2.txt 
--- ЛР2/dot_1.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF 
-cls 
-echo list of hidden files in a folder %1 in the file %2
-dir %1 /ah /b > D:\Универ\1 курс\2 семестр\ОС\ЛР2\%2.txt 
